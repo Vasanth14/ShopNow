36fcdfbf5269a26f67cc71954ae7f088e8eec9ad
Vasanth Chandrasekar <35191203+Vasanth14@users.noreply.github.com>
2024-04-10 13:42:47 +0530

    clearing haso laptop

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,8 +1,15 @@
+// const notFound = (req, res, next) => {
+//     const error = new Error(`Not found - ${req.originalUrl}`)
+//     res.status(404)
+//     next(error)
+// }
+
 const notFound = (req, res, next) => {
-    const error = new Error(`Not found - ${req.originalUrl}`)
-    res.status(404)
-    next(error)
-}
+    res.status(404).json({
+        message: `Not found - ${req.originalUrl}`
+    });
+};
+
 
 const errorHandler = (err, req, res, next) => {
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode
